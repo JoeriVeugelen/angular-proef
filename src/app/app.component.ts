43e80acacfc90dcf69ca34d75d3b5a368ba478bc
Joeri <56969837+JoeriVeugelen@users.noreply.github.com>
2024-11-13 18:05:16 +0100

    Added text block with buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SmileyFeedbackComponent } from './smiley-feedback/smiley-feedback.component';
+import { TextBlockComponent } from './text-block/text-block.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SmileyFeedbackComponent],
+  imports: [RouterOutlet, SmileyFeedbackComponent, TextBlockComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
