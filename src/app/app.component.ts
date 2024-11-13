1513790c32f37f331bb6a66ba97921605b8cdf78
Joeri <56969837+JoeriVeugelen@users.noreply.github.com>
2024-11-13 18:30:24 +0100

    Added blog component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SmileyFeedbackComponent } from './smiley-feedback/smiley-feedback.component';
 import { TextBlockComponent } from './text-block/text-block.component';
+import { BlogComponent } from './blog/blog.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SmileyFeedbackComponent, TextBlockComponent],
+  imports: [
+    RouterOutlet,
+    SmileyFeedbackComponent,
+    TextBlockComponent,
+    BlogComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
